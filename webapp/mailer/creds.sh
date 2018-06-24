88c8710cd25b1b13c289cbab4091ca284d62e1fb
Budi <bigpow@gmail.com>
2018-06-24 00:04:20 -0700

    code & file housekeeping

--- webapp/mailer/creds.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export MAILGUN_ACCOUNT=sandbox0686c7cac302423c91f796b508d3c33d
-export MAILGUN_KEY=ed219b96e1411ae00c1aeb4bc6c09b42-0470a1f7-27d0be25
-export MAILGUN_TO=bigpow@gmail.com
--- webapp/mailer/mailer.py
+++ webapp/mailer/mailer.py
@@ -4,9 +4,9 @@ import urllib.parse
 
 class Mailer(object):
     """
-    Mailer class
+    Mailer class wrapper for MailGun API
 
-    Keywords/parameters:
+    Keys/parameters:
     ====================
     {
         'account': '',
@@ -21,12 +21,11 @@ class Mailer(object):
     """
 
     def __init__(self, **kwargs):
-        # Mail header
+        # Mail data
         self.account = kwargs['account']
         self.key = kwargs['key']
         self.url = kwargs['url']
         self.addr_from = kwargs['addr_from']
-        # Mail data
         self.addr_to = kwargs['addr_to']
         self.mail_subject = kwargs['mail_subject']
         self.mail_text = kwargs['mail_text']
@@ -70,18 +69,10 @@ def main():
     Mailer Test: Send test email
     Command-line executed
 
-    1) Load MailGun credentials:
-        a) from json
-        b) from env
-    1') Load from address & URL
-    2) Load email address to spam:
-        a) from textfile
-        b) from env
-        * Otherwise, would be passed on from other module(s)
-    3) Load dummy email data:
-        a) from text
-        b) from html
-        * Otherwise, would be passed on from other module(s)
+    1) Load MailGun credentials from env
+    2) Load from_address & URL
+    3) Load email address to spam from env
+    4) Load dummy bulk email data from html template
     4) Wrap the package (dictionary)
         mail_data = {
             'account': '',
@@ -95,82 +86,23 @@ def main():
         }
     5) Send the package to mailer
     """
-    DEBUG = False
-    # 1) Load MailGun credentials:
-    #     a) from json
-    import json
-    # with open('creds.json', 'r') as f:
-    #     creds_json = json.load(f)
-    # if DEBUG:
-    #     print("{}:\n{}".format('creds.json', json.dumps(creds_json, indent=2)))
-    #     b) from env
     import os
-    creds_env = {
-        'account': os.getenv('MAILGUN_ACCOUNT'),
-        'key': os.getenv('MAILGUN_KEY')
-    }
-    if DEBUG:
-        print("{}:\n{}".format('env', json.dumps(creds_env, indent=2)))
 
-    # 1') Load from address & URL
-    url = 'https://api.mailgun.net/v3/{0}.mailgun.org/messages'.format(
-        creds_env['account']
-    )
-    addr_from = "user <user@{0}.mailgun.org>".format(creds_env['account'])
-
-    # 2) Load email address to spam:
-    #     a) from textfile
-    # emailto_text = {
-    #     'addr_to': open('mail_to.txt', 'r').read()
-    # }
-    # if DEBUG:
-    #     print("{}:\n{}".format(
-    #         'mail_to.txt',
-    #         json.dumps(emailto_text, indent=2))
-    #         )
-    #     b) from env
-    emailto_env = {
-        'addr_to': os.getenv('MAILGUN_TO')
-    }
-    if DEBUG:
-        print("{}:\n{}".format('env', json.dumps(emailto_env, indent=2)))
-
-    # 3) Load dummy email data:
-    #     a) from text
-    mail_payload_text = {
-        'mail_text': open('mail_payload_simple.txt', 'r').read()
-    }
-    if DEBUG:
-        print("{}:\n{}".format(
-            'mail_payload_simple.txt',
-            json.dumps(mail_payload_text, indent=2))
-        )
-    # 3) Load dummy email data:
-    #     b) from html
-    mail_payload_html = {
-        'mail_html': open('mail_payload_simple.html', 'r').read()
-    }
-    if DEBUG:
-        print("{}:\n{}".format(
-            'mail_payload_simple.html',
-            json.dumps(mail_payload_html, indent=2))
-        )
-
-    # 4) Wrap the package
     mail_data = {
-        'account': creds_env['account'],
-        'key': creds_env['key'],
-        'url': url,
-        'addr_from': addr_from,
-        'addr_to': emailto_env['addr_to'],
+        'account': os.getenv('MAILGUN_ACCOUNT'),
+        'key': os.getenv('MAILGUN_KEY'),
+        'url': 'https://api.mailgun.net/v3/{0}.mailgun.org/messages'.format(
+            os.getenv('MAILGUN_ACCOUNT')
+            ),
+        'addr_from': "user <user@{0}.mailgun.org>".format(
+            os.getenv('MAILGUN_ACCOUNT')
+            ),
+        'addr_to': os.getenv('MAILGUN_TO'),
         'mail_subject': 'Test Email',
-        'mail_text': '',
-        'mail_html': mail_payload_html['mail_html'],
+        'mail_text': 'This is a test email',
+        'mail_html': open('templates_email/passion/template.html', 'r').read(),
     }
-    if DEBUG:
-        print("{}:\n{}".format('mail_data', json.dumps(mail_data, indent=2)))
 
-    # 5) Send the package to mailer
     mailer = Mailer(**mail_data)
     mailer.send_email()
     if 1:
