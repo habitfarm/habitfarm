3f04bc7e6c6b734d9ebfc00bca9a0dc3485de8e3
Hien Phuong <emails.hienn@gmail.com>
2018-06-26 11:35:01 -0700

    Import LogEntry and Habit from models.py to views.py

--- webapp/habits/views.py
+++ webapp/habits/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from webapp.models import LogEntry, Habit
 
 def habit_list(request):
     """
