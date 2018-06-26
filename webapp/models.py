001aa19d9d5dbe5db34339b99270855c9cc32fa1
Hien Phuong <emails.hienn@gmail.com>
2018-06-25 21:51:05 -0700

    resolved merge conflict

--- webapp/models.py
+++ webapp/models.py
@@ -1,29 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-
+from colorfield.fields import ColorField
 
 class Habit(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    current_streak = models.PositiveIntegerField()
-    longest_streak = models.PositiveIntegerField()
-    last_log_entry = models.PositiveIntegerField()
+    schedule = models.CharField(
+        default='daily',
+        max_length = 20,
+        choices = [
+            ('daily', 'Daily'),
+            ('weekly', 'Weekly'),
+            ('monthly', 'Monthly'),
+        ],
+    )
+    current_streak = models.PositiveIntegerField(default=0)
+    longest_streak = models.PositiveIntegerField(default=0)
+    last_log_entry = models.DateTimeField(null=True)
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length = 20,
         choices = [('open', 'Open'), ('achieved', 'Achieved'),],
     )
-    color = models.CharField(max_length=100)
+    color = ColorField(
+        max_length=100,
+        default='#663399',
+        null=True
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE
     )
 
 class LogEntry(models.Model):
-    note = models.TextField()
-    logged = models.DateTimeField()
+    note = models.TextField(null=True)
+    logged = models.DateTimeField(
+        null=False,
+        blank=False,
+    )
     created = models.DateTimeField(auto_now_add=True)
     habit = models.ForeignKey(
         Habit,
